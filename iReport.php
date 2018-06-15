7816268ad13fb4b6a943dc61ffad96198adaa16e
Anderson Silva <anderson@fazti.com.br>
2018-06-15 11:58:42 -0300

    Namespace

--- iReport.php
+++ iReport.php
@@ -6,7 +6,7 @@
  * @license   MIT License - view the LICENSE file that was distributed with this source code.
  */
 
-namespace diskingressos\iReport;
+namespace diskingressos\yii2ireport;
 
 include_once('class/fpdf/fpdf.php');
 include_once('class/PHPJasperXML.inc');
